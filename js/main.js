7436dbbc1b34374dd2b54ee2403ba10c6f011091
Jaehyeok <lionjk92.naver.com>
2020-07-18 11:01:05 +0900

    Change connetorbox effect, bestconnector img and Add review best img, app section and Apply script

--- js/main.js
+++ js/main.js
@@ -3,6 +3,9 @@ $(document).ready(function(){
   connectboxEffect(); // 커넥트박스영역 슬라이드 효과
   resultCountEffect(); // 성과영역 카운트효과
   connectorSlideEffect(); //베스트커넥터영역 슬라이드 효과
+  reviewParallaxEffect() // 리뷰영역 패러랙스스크롤 효과와 큰리뷰영역 활성화 효과 
+  subVisualEffect(); // 서브비주얼 효과
+  appAreaslideEffect(); // 앱영역 슬라이드 효과
 })
 
 function mainVisualEffect(){
@@ -47,36 +50,39 @@ function mainVisualEffect(){
   }
 }
 function connectboxEffect(){
-  var $movingbox=$('.connectbox_inner');
-
+  var $movingbox=$('.moving_box');
+  var $fixedBox=$(".fixed_box");
   // 이벤트
   $('.connect_left_btn').on('click',slideLeft); // 전문커넥터버튼클릭이벤트
   
   // 함수
   function slideLeft(){ //전문커넥터영역 활성화 함수
-    $movingbox.stop().animate({'left':-800},500,'easeOutCubic',function(){
+    $movingbox.css({"z-index":1});
+    $fixedBox.css({'z-index':0});
+    $movingbox.stop().animate({'left':-1200},500,'easeOutCubic',function(){
+      $fixedBox.css({"left":-800}) // 가리면서 사라지는 효과 후 다음 이벤트를 위해 다시 배치
       $('.connect_right_btn').on('click',slideRight); //html로 새로 넣어줬기 때문에 이벤트 다시 지정해주는게 필요
+
     });
-    $('.connect_title_box').html('<p class="title_box_title">일반 커넥터</p><p class="title_box_text">구매 대행부터 까지 해외 현지의<br>필요한 요구사항을 커넥터가<br>대신 해드립니다.</p><div class="connect_right_btn"><a href="#;"></a></div>')
-    $('.connect_title_box').addClass('box_selected'); //백그라운드 컬러 변경을 위한 클래스 추가
-    $('.normal_connect_wrap').stop().animate({'opacity':0},0,'easeOutCubic');
+    $('.normal_connect_wrap').stop().animate({'opacity':0},500,'easeOutCubic');
     $('.pro_connect_wrap').stop().animate({'opacity':1},1000,'easeOutCubic');
   }
   function slideRight(){ //일반커넥터영역 활성화 함수
-    $movingbox.stop().animate({'left':0},500,'easeOutCubic',function(){
+    $movingbox.css({'z-index':0});
+    $fixedBox.css({'z-index':1});
+    $fixedBox.stop().animate({'left':0},500,'easeOutCubic',function(){
+      $movingbox.css({"left":0}) // 가리면서 사라지는 효과 후 다음 이벤트를 위해 다시 배치
       $('.connect_left_btn').on('click',slideLeft); //html로 새로 넣어줬기 때문에 이벤트 다시 지정해주는게 필요
     })
-    $('.connect_title_box').html('<p class="title_box_title">전문 커넥터</p><p class="title_box_text">전문성을 가진 커넥터가<br>시장조사, 부동산거래, 법률문제 등<br>현지의 전문적인 업무를 대신 해드립니다.</p><div class="connect_left_btn"><a href="#;"></a></div>')
-    $('.connect_title_box').removeClass('box_selected');
     $('.normal_connect_wrap').stop().animate({'opacity':1},1000,'easeOutCubic');
-    $('.pro_connect_wrap').stop().animate({'opacity':0},0,'easeOutCubic');
+    $('.pro_connect_wrap').stop().animate({'opacity':0},500,'easeOutCubic');
   }
 }
 function resultCountEffect(){
   var startTop=$("#count_wrap").offset().top*0.5;
   var scrollHeight;
   var isdone=false;
-
+  
   // 이벤트
   $(window).on('scroll',onScroll)
 
@@ -116,28 +122,108 @@ function resultCountEffect(){
   }
 }
 function connectorSlideEffect(){
-  var $slideWrap=$(".best_connector_list");
-  var $connectorLi=$slideWrap.children("li");
+  var $slideWrap=$('.best_connector_list');
+  var $connectorLi=$slideWrap.children('li');
   var listWidth=$connectorLi.innerWidth();
-  var marginX=parseInt($connectorLi.css("margin-right")); // 값이 px로 나와서 number로 변환 필요
+  var marginX=parseInt($connectorLi.css('margin-right')); // 값이 px로 나와서 number로 변환 필요
   var movingX=(listWidth*3)+(marginX*3)+(4*3); // 슬라이드 범위 
   var currentX;
 
   // 이벤트
-  $(".best_left_btn").on("click",slideLeft); // 왼쪽버튼 클릭 이벤트
-  $(".best_right_btn").on("click",slideRight); // 오른쪽버튼 클릭 이벤트
+  $('.best_left_btn').on('click',slideLeft); // 왼쪽버튼 클릭 이벤트
+  $('.best_right_btn').on('click',slideRight); // 오른쪽버튼 클릭 이벤트
 
   // 함수
   function slideLeft(){ //왼쪽버튼 클릭함수
     currentX=$slideWrap.position().left;
     if(currentX<0){
-      $('.best_connector_list:not(:animated)').animate({"left":currentX+movingX},500,"easeOutCubic") // 연속클릭 시 좌표 불균형을 방지하기 위해 notanimate로 지정
+      $('.best_connector_list:not(:animated)').animate({'left':currentX+movingX},500,'easeOutCubic') // 연속클릭 시 좌표 불균형을 방지하기 위해 notanimate로 지정
     }
   }
   function slideRight(){ //오른쪽버튼 클릭함수
     currentX=$slideWrap.position().left;
     if(currentX>-movingX){
-      $('.best_connector_list:not(:animated)').animate({"left":currentX-movingX},500,"easeOutCubic") // 연속클릭 시 좌표 불균형을 방지하기 위해 notanimate로 지정
+      $('.best_connector_list:not(:animated)').animate({'left':currentX-movingX},500,'easeOutCubic') // 연속클릭 시 좌표 불균형을 방지하기 위해 notanimate로 지정
     }
+  }
+}
+function reviewParallaxEffect(){
+  var $reviewWrap=$("#review_wrap");
+  var $leftWrap=$(".review_left_wrap");
+  var startTop=$reviewWrap.offset().top;
+  var endTop=startTop+$reviewWrap.innerHeight()-$leftWrap.innerHeight();
+  var scrollTop=$(document).scrollTop();
+  // 이벤트
+  $(window).on('scroll',onScroll); //리뷰패러랙스 이벤트
+
+  // 함수
+  function onScroll(){ //리뷰 패러렉스 함수
+    var newTop=$(document).scrollTop();
+    if(newTop>=startTop && newTop<=endTop){
+      $leftWrap.css({"top":newTop-startTop})
+    }
+    scrollTop=newTop;
+  }
+}
+function subVisualEffect(){
+  var $movingWrap=$('.sub_test_list');
+  var $textList=$('.sub_test_list').children('li');
+  var $visualWrap=$('.sub_visual_list');
+  var $visualList=$visualWrap.children('li');
+  var listNum=$textList.size();
+  var movingX=$textList.innerWidth();
+  var clickIndex=0;
+
+  // 이벤트
+  $('.sub_left_btn').on('click',prevSlide); //왼쪽버튼 클릭 이벤트
+  $('.sub_right_btn').on('click',nextSlide); // 오른쪽버튼 클릭 이벤트
+
+  // 함수
+  function prevSlide(){
+    if(clickIndex>0){
+      clickIndex--
+      $movingWrap.css({'left':-(movingX*clickIndex)})
+      $visualList.last().prependTo($visualWrap); // 마지막리스트를 첫번째위치로 이동
+      $visualList=$visualWrap.children('li');
+      $('.paging').text(clickIndex+1)
+    }
+  }
+  function nextSlide(){
+    if(clickIndex<listNum-1){
+      clickIndex++
+      $movingWrap.css({'left':-(movingX*clickIndex)})
+      $visualList.first().appendTo($visualWrap); // 첫번째리스트를 마지막위치로 이동
+      $visualList=$visualWrap.children('li');
+      $('.paging').text(clickIndex+1)
+    }
+  }
+}
+function appAreaslideEffect(){
+  var $movingWrap=$('.inner_img_list');
+  var movingX=$(".inner_img_list").children('li').innerWidth();
+  var $dotList=$('.app_dot_list').children('li')
+  var dotIndex=0;
+
+  $dotList.eq(dotIndex).addClass('active_dot')
+  var timer = setInterval(autoPlay,3000); // 오토플레이 실행
+
+  //이벤트
+  $dotList.on('click',clickDot); // 도트 클릭시 이벤트
+
+  //함수
+  function autoPlay(){ //오토플레이 함수
+    dotIndex++
+    if(dotIndex>=3){
+      dotIndex=0;
+    }
+      $movingWrap.stop().animate({'left':-movingX*dotIndex},500,"easeOutCubic")
+      $dotList.removeClass('active_dot')
+      $dotList.eq(dotIndex).addClass('active_dot')
+  }
+  function clickDot(){ // 도트 클릭시 슬라이드 함수
+    dotIndex=$dotList.index($(this));
+    $movingWrap.stop().animate({'left':-movingX*dotIndex},500,"easeOutCubic")
+    $dotList.removeClass('active_dot')
+    $dotList.eq(dotIndex).addClass('active_dot')
   }
 }
